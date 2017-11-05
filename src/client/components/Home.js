03a39579b5ff2217a87be011bd14cb52100ac53d
asmm.dan <asmm.dan@gmail.com>
2017-11-05 12:54:45 -0800

    complete boilerplate

---
+++ src/client/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <div>React Server-Side-Rendering BoilerPlate</div>
+      <button onClick={() => console.log('SSR BoilerPlate!')}>React!</button>
+    </div>
+  );
+};
+export default Home;
